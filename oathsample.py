584a9e61bc6f30bdb9395408812a6c18537d0d04
Cosmo Kramer <kelvind.kramer@kelvind.com>
2021-04-22 15:35:23 +0000

    Resolves Issue MISC-PYTHON-T-2

--- oathsample.py
+++ oathsample.py
@@ -21,4 +21,4 @@ api_call_headers = {
 }
 api_call_response = requests.get(test_api_url, headers=api_call_headers, verify=True)
 print(api_call_response.text)
-print('Monkey, monkey, monkey!')
+print('Monkey, monkey, orange juice!')

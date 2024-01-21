cc2e63a3326262578a48f8326f5d1b4604f6a8fe
kelvin.olson <kelvin.olson@centreviews.com>
2024-01-21 11:21:44 -0600

    Addresses as arguments, threshold 1 hr

--- aprsStaleCheck.py
+++ aprsStaleCheck.py
@@ -5,13 +5,13 @@ import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
-def send_email(alert_message, is_alert, timestamp_str, smtp_password):
+def send_email(alert_message, is_alert, timestamp_str, smtp_password, from_address, to_address, station):
     print("Preparing email.")
-    alert_subject = "ALERT:" if is_alert else ""
+    alert_subject = "ALERT: " if is_alert else ""
     msg = MIMEMultipart()
-    msg['Subject'] = f"{alert_subject} {alert_message}"
-    msg['From'] = 'kelvind@kelvind.com'
-    msg['To'] = 'kelvin.d.olson@me.com'
+    msg['Subject'] = f"{alert_subject} {station} checked: {alert_message}"
+    msg['From'] = from_address
+    msg['To'] = to_address
     body = f"{alert_message}\nTimestamp: {timestamp_str}"
     msg.attach(MIMEText(body, 'plain'))
 
@@ -19,28 +19,31 @@ def send_email(alert_message, is_alert, timestamp_str, smtp_password):
     # Send the message via the ProtonMail SMTP server on localhost.
     # ProtonMail Bridge should be running.
     with smtplib.SMTP('127.0.0.1', 1025) as s:
-        s.login('kelvind@kelvind.com', smtp_password)
+        s.login(from_address, smtp_password)
         s.send_message(msg)
     print("Email sent.")
 
 if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        print("Usage: aprsStaleCheck.py <station> <api_key> <smtp_password>")
+    if len(sys.argv) != 6:
+        print("Usage: aprsStaleCheck.py <station> <api_key> <smtp_password> <from_address> <to_address>")
         exit(1)
 
-    target = sys.argv[1]
+    station = sys.argv[1]
     api_key = sys.argv[2]
-    smtp_password = sys.argv[3]  # SMTP password is accepted via command line arguments.
+    smtp_password = sys.argv[3]
+    from_address = sys.argv[4]
+    to_address = sys.argv[5]
+
     url = f"https://api.aprs.fi/api/get"
 
     params = {
-        'name': target,
+        'name': station,
         'what': 'loc',
         'apikey': api_key,
         'format': 'json',
     }
 
-    print(f"Fetching data for station {target}")
+    print(f"Fetching data for station {station}")
     response = requests.get(url, params=params)
 
     if response.status_code != 200:
@@ -50,21 +53,19 @@ if __name__ == "__main__":
     data = response.json()
 
     if 'entries' in data and len(data['entries']) > 0:
-        print(f"Data fetched, assessing lasttime for station {target}")
-        # Getting lasttime from data and converting it to datetime
+        print(f"Data fetched, assessing lasttime for station {station}")
+
         lasttime = int(data['entries'][0]['lasttime'])
         lasttime_date = datetime.fromtimestamp(lasttime)
 
-        # Formatting lasttime according to YYYY-MM-DD hh:mm:ss
         formatted_lasttime = lasttime_date.strftime('%Y-%m-%d %H:%M:%S')
 
-        # Checking if lasttime was more than 600 seconds ago
-        alert = datetime.now() - lasttime_date > timedelta(seconds=600)
-        message = f"Lasttime ({formatted_lasttime}) was more than 600 seconds ago." if alert else f"Lasttime is within the last 600 seconds: {formatted_lasttime}"
-        print(message)
+        alert = datetime.now() - lasttime_date > timedelta(seconds=3600)
+        message = f"Lasttime ({formatted_lasttime}) was more than 3600 seconds ago." if alert else f"Lasttime ({formatted_lasttime}) is within the last 3600 seconds."
 
-        send_email(message, alert, formatted_lasttime, smtp_password)
+        print(message)
+        send_email(message, alert, formatted_lasttime, smtp_password, from_address, to_address, station)
     else:
-        print(f"No data returned for station {target}.")
-        message = f"No data returned for station {target}. Please verify the station id."
-        send_email(message, True, '', smtp_password)
+        print(f"No data returned for station {station}.")
+        message = f"No data returned for station {station}. Please verify the station id."
+        send_email(message, True, '', smtp_password, from_address, to_address, station)
